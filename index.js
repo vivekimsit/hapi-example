b1b11b347ef1cf818e277d1998aa299dcda8c42e
Vivek Poddar <vivekimsit@gmail.com>
2017-06-12 13:11:37 +0530

    * Reading cookie * Encrypting cookie

--- index.js
+++ index.js
@@ -8,14 +8,22 @@ server.connection({
   port: 8000
 });
 
+server.state('hello', {
+  ttl: 60 * 60 * 1000,
+  isHttpOnly: true,
+  isSecure: false,
+  encoding: 'iron',
+  password: 'a5LewP10pXNbWUdYQakUfVlk1jUVuLuUU6E1WEE302k'
+})
+
 server.route({
   method: 'GET',
   path: '/',
   config: {
     handler: function(request, reply) {
-      const hello = request.state.hello;
-      reply(`Cookies! ${hello}`)
-        .state('hello', 'world', {isSecure: false}); // isSecure=false so that its visible in devtools
+      const hello = request.state.hello ? request.state.hello.name : 'guest';
+      reply(`Cookies, ${hello}`)
+        .state('hello', {name: 'foo'});
     }
   }
 });
